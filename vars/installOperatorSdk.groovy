c8711199172ea46e5f6760ba807436e5fb5c5040
Pavel Šturc <psturc@redhat.com>
2019-07-16 15:05:21 +0200

    feat: install operator-sdk - output dir parameter

--- vars/installOperatorSdk.groovy
+++ vars/installOperatorSdk.groovy
@@ -2,12 +2,16 @@
 
 /** Download Operator SDK binary and save it to location within PATH
  *
- * @param operatorSdkVersion version from https://github.com/operator-framework/operator-sdk/releases, e.g. "v0.9.0"
- * 
+ * @param operatorSdkVersion    version from https://github.com/operator-framework/operator-sdk/releases, e.g. "v0.9.0"
+ * @param outputDirectory       absolute path to folder which the binary should be downloaded to
+ *
 */
-def call(String operatorSdkVersion) {
+def call(Map params) {
+    final String outputDirectory = params.outputDirectory ?: "/usr/local/bin"
+    final String operatorSdkVersion = params.operatorSdkVersion
+
     sh """
-    sudo curl -Lso /usr/local/bin/operator-sdk https://github.com/operator-framework/operator-sdk/releases/download/${operatorSdkVersion}/operator-sdk-${operatorSdkVersion}-x86_64-linux-gnu
-    sudo chmod +x /usr/local/bin/operator-sdk
+    sudo curl -Lso ${outputDirectory}/operator-sdk https://github.com/operator-framework/operator-sdk/releases/download/${operatorSdkVersion}/operator-sdk-${operatorSdkVersion}-x86_64-linux-gnu
+    sudo chmod +x ${outputDirectory}/operator-sdk
     """
 }
