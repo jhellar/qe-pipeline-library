94de4519221a33f45354e28510465ae7bf310a9f
Pavel Šturc <psturc@redhat.com>
2019-07-16 15:09:58 +0200

    fix: operatorSdkVersion -> version

--- vars/installOperatorSdk.groovy
+++ vars/installOperatorSdk.groovy
@@ -2,16 +2,16 @@
 
 /** Download Operator SDK binary and save it to location within PATH
  *
- * @param operatorSdkVersion    version from https://github.com/operator-framework/operator-sdk/releases, e.g. "v0.9.0"
- * @param outputDirectory       absolute path to folder which the binary should be downloaded to
+ * @param version           version from https://github.com/operator-framework/operator-sdk/releases, e.g. "v0.9.0"
+ * @param outputDirectory   absolute path to folder which the binary should be downloaded to
  *
 */
 def call(Map params) {
     final String outputDirectory = params.outputDirectory ?: "/usr/local/bin"
-    final String operatorSdkVersion = params.operatorSdkVersion
+    final String version = params.version
 
     sh """
-    sudo curl -Lso ${outputDirectory}/operator-sdk https://github.com/operator-framework/operator-sdk/releases/download/${operatorSdkVersion}/operator-sdk-${operatorSdkVersion}-x86_64-linux-gnu
+    sudo curl -Lso ${outputDirectory}/operator-sdk https://github.com/operator-framework/operator-sdk/releases/download/${version}/operator-sdk-${version}-x86_64-linux-gnu
     sudo chmod +x ${outputDirectory}/operator-sdk
     """
 }
