20230c9889c4997a0d84efa771449457a379ab92
Pavel Šturc <psturc@redhat.com>
2019-09-13 21:54:05 +0200

    feat: operator test - support ns & global manifest

--- vars/runOperatorTestWithImage.groovy
+++ vars/runOperatorTestWithImage.groovy
@@ -5,9 +5,39 @@ def call(Map params) {
 
     final String containerImageName = params.containerImageName
     final String namespace = params.namespace ?: sh(script: "oc project -q", returnStdout: true).trim()
+    final Boolean namespacedManifest = params.namespacedManifest ?: false
+    final Boolean globalManifest = params.globalManifest ?: false
+    String namespacedManifestFilename = "''"
+    String globalManifestFilename = "''"
+
+    if (namespacedManifest) {
+        namespacedManifestFilename = "namespaced_manifest.yaml"
+        sh """
+        for file in \$(find deploy -name "*" -not -path 'deploy/**/*' | grep -E 'service|role|operator'); 
+        do
+            echo '---' >> ${namespacedManifestFilename}
+            cat \$file >> ${namespacedManifestFilename}
+        done
+        """
+    }
+
+    if (globalManifest) {
+        globalManifestFilename = "global_manifest.yaml"
+        sh """
+        for file in \$(find deploy -name "*_crd*");
+        do
+            echo '---' >> ${globalManifestFilename}
+            cat \$file >> ${globalManifestFilename}
+        done
+        """
+    }
 
     sh """
     yq w -i deploy/operator.yaml spec.template.spec.containers[0].image ${containerImageName}
-    operator-sdk test local ./test/e2e --namespace ${namespace} --go-test-flags '-v'
+    operator-sdk test local ./test/e2e \
+        --namespace ${namespace} \
+        --namespaced-manifest ${namespacedManifestFilename} \
+        --global-manifest ${globalManifestFilename} \
+        --go-test-flags '-v'
     """
 }
